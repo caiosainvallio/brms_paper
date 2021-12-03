f0c860a361de646eecf4a3b65508d56a416f3f61
Caio Sain Vallio <caiosainvallio@me.com>
2021-12-03 14:51:20 -0300

    Add files via upload

---
+++ brms_paper_codes.R
@@ -0,0 +1,39 @@
+##############################################################################
+#     brms: An R Package for Bayesian Multilevel Models Using Stan           #
+#                        Paul-Christian Burkner                              #
+##############################################################################
+
+# package and dataset
+library(brms)
+options(mc.cores = parallel::detectCores())
+
+data("kidney", package = "brms")
+head(kidney)
+
+
+# fit model
+fit1 <- brm(
+  formula = time | cens(censored) ~ age * sex + disease + (1 + age|patient),
+  data = kidney, family = lognormal(),
+  prior = c(
+    set_prior("normal(0,5)", class = "b"),
+    set_prior("cauchy(0,2)", class = "sd"),
+    set_prior("lkj(2)", class = "cor")
+  ),
+  warmup = 1000,
+  iter = 2000,
+  chains = 4,
+  control = list(adapt_delta = 0.95)
+)
+
+
+# Group-level terms are of the form (coefs | group), where coefs
+# contains one or more variables whose effects are assumed to vary with the levels of the grouping
+# factor given in group. Multiple grouping factors each with multiple group-level coefficients
+# are possible. In the present example, only one group-level term is specified in which 1 + age
+# are the coefficients varying with the grouping factor patient. This implies that the intercept
+# of the model as well as the effect of age is supposed to vary between patients. By default,
+# group-level coefficients within a grouping factor are assumed to be correlated.
+
+
+
