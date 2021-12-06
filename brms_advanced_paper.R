d01986afe5a5aaff0a0c634a4972978a6e910303
Caio Sain Vallio <caiosainvallio@me.com>
2021-12-06 11:58:58 -0300

    Add files via upload

--- brms_advanced_paper.R
+++ brms_advanced_paper.R
@@ -17,5 +17,17 @@ help("set_prior")
 
 # https://ben18785.shinyapps.io/distribution-zoo/
   
+
+# multilevel formula syntax ----------------------------------------------------
   
-  
+
+# response ~ pterms + (gterms | group)
+
+
+# `pterms` contain population-level effects, assumed to be the same 
+# across observations.
+
+# `gterms` contain so called group-level effects, assumed to vary across 
+# the grouping variables specified in group
+
+
