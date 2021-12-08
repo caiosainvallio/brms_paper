04df6881f6e85f945b7bc2c99b7cec03464720b5
Caio Sain Vallio <caiosainvallio@me.com>
2021-12-08 17:37:26 -0300

    Add files via upload

--- mixed_effects_sushmita.R
+++ mixed_effects_sushmita.R
@@ -36,6 +36,80 @@
 # often a random effect.
 
 
+## NOTE ----------------------------------------------------------------------
+# Predictor variables can be both fixed (i.e. causing a main effect/interaction)
+# and random (i.e. causing variance/variability in responses). When building 
+# your models, you can treat your predictor as a fixed & random factor.
+
+
+
+## Intercepts ----------------------------------------------------------------
+# The baseline relationship between Independent Variables & Dependent Variable.
+# Fixed effects are plotted as intercepts to reflect the baseline level of 
+# your Dependent Variable.
+
+## Random intercepts: Variability in baseline measurements
+## Fixed intercepts: Baseline variance is not affected
+
+
+## Slope ---------------------------------------------------------------------
+# The strength of the relationship between Independent Variables & Dependent
+# Variable (controlling for randomness), which represent random effects. 
+# You should expect to see differences in the slopes of your random factors.
+
+
+
+# Setting up -----------------------------------------------------------------
+
+library (lmerTest) # Mixed model package by Douglas Bates, comes w/ pvalues! 
+library (texreg) #Helps us make tables of the mixed models
+library (afex) # Easy ANOVA package to compare model fits
+library (plyr) # Data manipulator package
+library (ggplot2) # GGplot package for visualizing data
+
+
+
+
+
+
+
+# Random effects structure ---------------------------------------------------
+
+## (1 | subject) -------------------------------------------------------------
+# Random intercepts and slopes for subjects (different baselines, different 
+# average effect per subject).
+
+
+## (1 + pizza | subject) -----------------------------------------------------
+# The effect of pizza will vary between subjects. Random intercepts for pizza 
+# consumption, random slopes for subjects influenced by pizza consumption.
+
+
+## (1 + pizza | subject) + (0 + time | subject) ------------------------------
+# Subjects have random intercepts and slopes as influenced by pizza consumption.
+# Time slopes can vary as function of the subject, but variance between pizza
+# consumption and time as independent
+
+
+## (1 + pizza + time | subject) ----------------------------------------------
+# Same as above, but variance between pizza consumption and time are SHARED 
+# (pizza consumption has relationship with time that varies by subject).
+
+
+## (1 + pizza * time | subject) ----------------------------------------------
+# Each subject can have their intercept, random slopes influenced by pizza 
+# and time, and their interaction between pizza and time. IMPORTANTLY, all 
+# random slopes and intercepts can be correlated.
+
+
+
+
+
+
+
+
+
+
 
 
 
