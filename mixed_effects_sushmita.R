453962be26eee9d449442fc16b9f5560a1d1ce07
Caio Sain Vallio <caiosainvallio@me.com>
2021-12-08 12:56:38 -0300

    Add files via upload

--- mixed_effects_sushmita.R
+++ mixed_effects_sushmita.R
@@ -5,6 +5,26 @@
 
 
 
+# When to Use? ---------------------------------------------------------------
+# Studies that obtain multiple measurements over time (longitudinal, 
+# time-series) or multiple trials per participant (within subjects) lend 
+# themselves well to mixed model analyses.
+
+
+# Important terminology ------------------------------------------------------
+
+## Crossed designs -----------------------------------------------------------
+# Refer to the within-subject variables (i.e. timepoint, condition, etc.). 
+# Crossed designs occur when multiple measurements are associated with multiple
+# grouping variables. In a completely crossed design, all subjects provide 
+# responses for all conditions/time-points.
+
+
+## Nested designs 
+# refer to the between-subject variable. Generally this is a higher-level 
+# variable that subjects or items are grouped under.
+
+
 
 
 
